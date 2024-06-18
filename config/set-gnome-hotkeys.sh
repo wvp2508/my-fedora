f85b7b894e3e8e96d0f75c6714a2d1938420af6b
Werner van Poeijer <werner@vanpoeijer.net>
2024-06-18 07:54:23 +0200

    first commit

---
+++ config/set-gnome-hotkeys.sh
@@ -0,0 +1,7 @@
+# Alt+F4 is very cumbersome
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super>w']"
+
+# Set flameshot (with the sh fix for starting under Wayland) on alternate print screen key
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'Flameshot'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'sh -c -- "flameshot gui"'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding '<Control>Print'
